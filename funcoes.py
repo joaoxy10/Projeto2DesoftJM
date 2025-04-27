a606b2b2699427b87fc466c5a49216e64b9cda7a
joaoxyz10 <joaovsm2@al.insper.edu.br>
2025-04-27 09:59:00 -0300

    terceiro comite

--- funcoes.py
+++ funcoes.py
@@ -23,9 +23,3 @@ def guardar_dado(rolados,estoque,guardar):
     resposta.append(estoque)
     return resposta
 
-
-dados_rolados = [1, 3, 2]
-dados_no_estoque = [1, 2]
-dado_para_guardar = 1
-
-print(guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar))
