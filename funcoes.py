25d11d65ff83ac89dcc2bb72f05810296393e460
joaoxyz10 <joaovsm2@al.insper.edu.br>
2025-04-27 11:21:35 -0300

    setimo commite

--- funcoes.py
+++ funcoes.py
@@ -54,3 +54,22 @@ def calcula_pontos_soma(lista):
         soma=soma+lista[i]
         i=i+1
     return soma
+
+def calcula_pontos_sequencia_baixa(lista):
+    i=0
+    lista_org=[]
+    numero=[1,2,3,4,5,6]
+    while i<len(numero):
+        i2=0
+        while i2 <len(lista):
+            if lista_org==[1,2,3,4] or lista_org==[2,3,4,5] or lista_org==[3,4,5,6]:
+                break
+            if numero[i]==lista[i2]:
+                if  not lista[i2] in lista_org :
+                    lista_org.append(lista[i2])
+            i2=i2+1
+        i=i+1
+    if lista_org==[1,2,3,4] or lista_org==[2,3,4,5] or lista_org==[3,4,5,6]:
+        return 15
+    else:
+        return 0
